bb238f1a4d05a813e5c21fb165b6ed5d69c7507d
bagheri-dev <bagheri.develop@gmail.com>
2024-10-28 09:34:07 +0330

    update UI

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -5,9 +5,6 @@ import { getWeather } from "../apis/fetchWeather";
 import { IWeatherData } from "../types/weatherData.Type";
 import { CiLocationOn } from "react-icons/ci";
 import { FaTemperatureEmpty } from "react-icons/fa6";
-import { WiHumidity } from "react-icons/wi";
-import { MdDisabledVisible, MdSpeed } from "react-icons/md";
-import { GiPressureCooker } from "react-icons/gi";
 
 const MapComponent: React.FC = () => {
   const [weatherData, setWeatherData] = useState<IWeatherData | null>(null);
@@ -27,7 +24,7 @@ const MapComponent: React.FC = () => {
   };
 
   return (
-    <div className="mt-10 overflow-hidden">
+    <div className="mt-10 flex gap-x-3 items-center">
       <MapContainer
         center={[35.6892, 51.389]}
         zoom={5}
@@ -42,7 +39,7 @@ const MapComponent: React.FC = () => {
 
       {weatherData && (
         <div className="flex flex-col justify-center mt-5 bg-gradient-to-l from-slate-800 to-violet-700 text-white rounded-xl py-4 px-6">
-          <h2 className="flex gap-x-3 font-bold text-3xl">
+          <h2 className="flex gap-x-3 font-bold text-2xl">
             Weather Information for {weatherData.name},{" "}
             {weatherData.sys.country}
             <CiLocationOn />
@@ -56,26 +53,8 @@ const MapComponent: React.FC = () => {
               />
             </div>
             <div className="flex items-center gap-x-4">
-              <FaTemperatureEmpty className="size-16" />
-              <p className="text-7xl">{weatherData.main.temp}°C</p>
-            </div>
-            <div className="flex items-center gap-x-8 mt-10">
-              <div className="flex flex-col items-center py-2 px-4 border rounded-lg gap-y-3">
-                <WiHumidity className="size-8" />
-                <p>Humidity: {weatherData.main.humidity}%</p>
-              </div>
-              <div className="flex flex-col items-center py-2 px-4 border rounded-lg gap-y-3">
-                <MdDisabledVisible className="size-8" />
-                <p>VISIBLITY {weatherData.visibility}</p>
-              </div>
-              <div className="flex flex-col items-center py-2 px-4 border rounded-lg gap-y-3">
-                <GiPressureCooker className="size-8" />
-                <p>Pressure: {weatherData.main.pressure} hPa</p>
-              </div>
-              <div className="flex flex-col items-center py-2 px-4 border rounded-lg gap-y-3">
-                <MdSpeed className="size-8" />
-                <p>Wind Speed: {weatherData.wind?.speed} m/s</p>
-              </div>
+              <FaTemperatureEmpty className="size-12" />
+              <p className="text-6xl">{weatherData.main.temp}°C</p>
             </div>
           </div>
         </div>
